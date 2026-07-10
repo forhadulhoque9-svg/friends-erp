7bdc0270cbdf51f485c8fd8d50c8cae890c49af8
forhadulhoque9-svg <forhadulhoque9@gmail.com>
2026-07-11 03:35:38 +0600

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div style={{ padding: '20px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Friends Enterprise ERP</h1>
+      <p>আপনার অফলাইন অ্যাপটি সফলভাবে তৈরি এবং সচল হয়েছে!</p>
+    </div>
+  </React.StrictMode>
+)
+
